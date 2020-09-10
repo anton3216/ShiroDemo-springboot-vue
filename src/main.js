b123be3eca42413fa155b68d13a79a6e00218fa7
dahuang <dahuang@cskj.com>
2020-09-10 17:47:39 +0800

    vue大黄第一次提交

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+import axios from 'axios'
+
+Vue.use(ViewUI)
+//注册axios
+Vue.prototype.$ajax = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
